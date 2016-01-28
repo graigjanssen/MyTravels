babf704724ec125e52dc5524986b9c7b2ae0f917
Graig Janssen <graig.janssen@gmail.com>
2016-01-28 14:05:06 -0500

    Create functionality for trips

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,15 +5,31 @@ app.controller('main', ['$scope', '$http', function($scope, $http){
   $scope.plannedTrips = [];
 
   $scope.radioClicked = null;
-  
+
+  function sortTrip(trip){
+    if (trip.visited){
+      $scope.pastTrips.push(trip);
+    } else {
+      $scope.plannedTrips.push(trip);
+    }
+  }
+
+  //Get all trips, sort into past and planned
   $http.get('api/trips').then(function(response){
     var allTrips = response.data.trips;
     allTrips.forEach(function(trip){
-      if (trip.visited){
-        $scope.pastTrips.push(trip);
-      } else {
-        $scope.plannedTrips.push(trip);
-      }
+      sortTrip(trip);
     });
   });
+
+  $scope.newTrip = {};
+
+  $scope.createTrip = function(){
+    $http.post('api/trips', { trip: $scope.newTrip }).then(function(response){
+      var trip = response.data;
+      sortTrip(trip);
+      $scope.newTrip = {};
+    });
+  };
+
 }]);
